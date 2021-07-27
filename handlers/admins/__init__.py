30762edd8c555ef893c9c2a5422313cba24dfe2b
ser4 <pristup2040@gmail.com>
2021-07-27 16:50:56 +1000

    finish delete_user.py

--- handlers/admins/__init__.py
+++ handlers/admins/__init__.py
@@ -1,5 +1,6 @@
 from .become_admin import dp
 from .add_user import dp
 from .see_all_users import dp
+from .delete_user import dp
 
 __all__ = ["dp"]
--- handlers/admins/delete_user.py
+++ handlers/admins/delete_user.py
@@ -2,8 +2,7 @@ from aiogram import types
 from aiogram.dispatcher.storage import FSMContext
 from aiogram.types import ReplyKeyboardRemove, CallbackQuery
 from loader import dp, db_of_active_users
-from keyboards.default import main_menu
-from states import AddUserByAdmin
+from states import DeleteUserByAdmin
 import data.config
 import datetime
 
@@ -22,7 +21,7 @@ def check_admin(user_id):
         return False
 
 
-@dp.message_handler(text='Добавить Участника')
+@dp.message_handler(text='Удалить Участника')
 async def add_user(message: types.Message, state: FSMContext):
     # добавления участника
     # Дынные: Telegram_id, Instagram_account_name, phone_number
@@ -33,77 +32,25 @@ async def add_user(message: types.Message, state: FSMContext):
         await message.answer(text=f'Вы не админ. telegram_id={id}')
         return
 
-    await AddUserByAdmin.state_id.set()
-    await message.answer('Добавления пользователя-1\n(Для отмены:_)\nВведите telegram_id:')
+    await DeleteUserByAdmin.choose_user.set()
+    await message.answer('Введите instagram_account участника(_ для отмены):')
 
 
-@dp.message_handler(state=AddUserByAdmin.state_id)
-async def add_user1_id(message: types.Message, state: FSMContext):
-    user_telegram_id = message.text
-
-    if user_telegram_id == '_':
-        await message.answer('Вы отменили добавление пользователя')
-        await state.finish()
-        return
-    try:
-        test1 = int(user_telegram_id)
-    except Exception as er:
-        await message.answer(f'Неверный формат telegram_id: {str(er)}')
-        await state.finish()
-        return
-
-    await state.update_data(telegram_id=user_telegram_id)
-    await message.answer('Добавления пользователя-2\n(Для отмены:_)\nВведите @instagram_account:')
-    await AddUserByAdmin.state_instagram_name.set()
-
-
-@dp.message_handler(state=AddUserByAdmin.state_instagram_name)
+@dp.message_handler(state=DeleteUserByAdmin.choose_user)
 async def add_user1_id(message: types.Message, state: FSMContext):
     instagram_account = message.text
 
     if instagram_account == '_':
-        await message.answer('Вы отменили добавление пользователя')
+        await message.answer('Вы отменили delete user')
         await state.finish()
         return
+    try:
+        db_of_active_users.delete_active_User_by_instagram_account_name(instagram_account)
+        await message.answer('Пользователь успешно удалён')
+    except Exception as er:
+        await message.answer('Не удалось удолить пользователя')
+        await message.answer(f'{er}')
 
-    await state.update_data(instagram_account=instagram_account)
-    await message.answer('Добавления пользователя-3\n(Для отмены:_ Если нет, то:1)\nВведите номер телефона:')
-    await AddUserByAdmin.state_phone_number.set()
-
-
-@dp.message_handler(state=AddUserByAdmin.state_phone_number)
-async def add_user1_id(message: types.Message, state: FSMContext):
-    phone_number = message.text
-
-    if phone_number == '_':
-        await message.answer('Вы отменили добавление пользователя')
-        await state.finish()
-        return
-    if str(phone_number) == '1':
-        phone_number = '123-no-phone'
-
-    data_ = await state.get_data()
-    telegram_id = data_.get('telegram_id')
-    instagram_account = data_.get('instagram_account')
-    phone_number = phone_number
-    time_today = str(datetime.datetime.today()).strip().split('.')[0]
-
-    f_text = f'''
-telegram_id: {telegram_id}
-instagram_account: {instagram_account}
-phone_number: {phone_number}
-time_today: {time_today}
-    '''
-    await message.answer(f_text)
-    await message.answer('Пользователь успешно добавлен!')
     await state.finish()
-    db_of_active_users.add_active_User(
-        telegram_id=telegram_id,
-        instagram_account_name=instagram_account,
-        phone_number=phone_number,
-        registration_date=time_today,
-
-    )
-
 
 
